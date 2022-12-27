463c9210382fba7159315160ea73ef7de565f144
Khalid Shawki <khalid.i.shawki@gmail.com>
2022-12-27 16:54:04 +0200

    add base server and configre jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-console.log("Hello, World!");
+import express from "express";
 
-let number;
+const app = express();
+const PORT = 3000;
 
+app.listen(PORT, (): void =>
+  console.log(`Running on http://localhost:${PORT}`)
+);
+
+app.get("/", (req, res): void => {
+  res.json({
+    message: `Hello, Khaled`,
+  });
+});
+
+export default app;
