00fc9aae5149fc815378c4c49433e9fe3d79f061
Khalid Shawki <khalid.i.shawki@gmail.com>
2022-12-28 23:20:17 +0200

    connect with db

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import config from "./config";
+import db from "./database";
+// import RateLimit from "express-rate-limit";
 
 const app = express();
-const PORT = 3000;
+const PORT = config.port || 3000;
 
+// Middleware request logger, secuirty, rate limitting
+app.use(morgan("common"), helmet(), express.json());
+
+// Create an instant from server
 app.listen(PORT, (): void =>
   console.log(`Running on http://localhost:${PORT}`)
 );
@@ -11,6 +20,26 @@ app.get("/", (req, res): void => {
   res.json({
     message: `Hello, Khaled`,
   });
+});
+
+app.post("/", (req, res): void => {
+  res.json({
+    message: `Hello, Khaled`,
+    data: req.body,
+  });
+});
+
+db.connect().then((client) => {
+  return client
+    .query("select now()")
+    .then((res) => {
+      client.release();
+      console.log(res.rows);
+    })
+    .catch((error) => {
+      client.release();
+      console.log(error.stack);
+    });
 });
 
 export default app;
