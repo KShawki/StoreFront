a9b8e62e3436c9e60667db596bb947f08fdf3ffc
Khalid Shawki <khalid.i.shawki@gmail.com>
2022-12-31 23:10:49 +0200

    compress all data into one file

--- migrations/sqls/20221231143242-product-table-up.sql
+++
@@ -1,7 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE products (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  price INTEGER NOT NULL,
-  category VARCHAR(50) NULL
-);
---
+++ migrations/sqls/20221231210801-main-up.sql
@@ -0,0 +1,33 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  id            uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  email         varchar(64) unique, 
+  username      varchar(64) not null,
+  first_name    varchar(64) not null, 
+  last_name     varchar(64) not null,
+  password      varchar(255) not null
+);  
+
+CREATE TABLE products (
+  id        SERIAL PRIMARY KEY,
+  name      VARCHAR(100) NOT NULL,
+  price     INTEGER NOT NULL,
+  category  VARCHAR(50) NULL
+);
+
+CREATE TABLE orders (
+  id      SERIAL PRIMARY KEY,
+  user_id NOT NULL REFERENCES users(id),
+  status  BOOLEAN NOT NULL
+);
+
+CREATE TABLE order_details (
+  order_id   INTEGER NOT NULL REFERENCES orders(id),
+  product_id INTEGER NOT NULL REFERENCES products(id),
+  quantity   INTEGER NOT NULL
+);
+
+
+
+
