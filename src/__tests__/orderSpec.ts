9bc85bc323dd4a757af800b663218b5d6fce76f4
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-01-17 13:29:33 +0200

    add test case

--- src/__tests__/orderSpec.ts
+++ src/__tests__/orderSpec.ts
@@ -3,8 +3,10 @@ import config from "../config";
 import User from "../types/user.type";
 import OrderModel from "../models/order.model";
 import { Order } from "../types/order.type";
+import ProductModel from "../models/product.model";
 
 const orderModel = new OrderModel();
+const productModel = new ProductModel();
 
 describe("orderModel TestSuite", () => {
   describe("==> Test Methods Exsist", () => {
@@ -31,6 +33,12 @@ describe("orderModel TestSuite", () => {
 });
 
 describe("Create Order Test Suite ", () => {
+  const product = {
+    name: "test",
+    price: 100,
+    category: "test",
+  };
+
   const order = {
     user_id: 1,
     product_id: 1,
@@ -38,15 +46,15 @@ describe("Create Order Test Suite ", () => {
   } as unknown as Order;
 
   beforeAll(async () => {
+    const createdProduct = await productModel.create(product);
     const createdOrder = await orderModel.create(order);
   });
 
   afterAll(async () => {
-    // const connection = await db.connect();
-    // const sql = `DELETE FROM orders;`;
-    // await connection.query(sql);
-    // connection.release();
-    console.log(`==> afterAll in async create order test suite was called`);
+    const connection = await db.connect();
+    const sql = `DELETE FROM orders where user_id = '${order.user_id}';`;
+    await connection.query(sql);
+    connection.release();
   });
 
   it("==> must return an order", async () => {
